89447dd3449f9b6e3eff6f947500b6e83c684067
Raúl Pleitez <racsoraul@gmail.com>
2014-07-19 23:16:32 -0600

    Agregadas las  librerías a usar en el proyecto: RXTXcomm.jar, jcommon-1.0.22.jar, jfreechart-1.0.18.jar

--- src/sv/ues/fia/graficadordatossensores/GraficadorDatosSensores.java
+++ src/sv/ues/fia/graficadordatossensores/GraficadorDatosSensores.java
@@ -7,10 +7,6 @@ public class GraficadorDatosSensores {
 
     public static void main(String[] args) {
         
-        Graficador_Frame graficador_Frame = new Graficador_Frame();
-        graficador_Frame.setVisible(true);
-        
-
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
@@ -21,13 +17,13 @@ public class GraficadorDatosSensores {
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(Graficador_Frame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-        //</editor-fold>
 
-        /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {
-                new Graficador_Frame().setVisible(true);
+                Graficador_Frame graficador_Frame = new Graficador_Frame();
+                graficador_Frame.setVisible(true);
+                graficador_Frame.setLocationRelativeTo(null);
             }
         });
     }
--- src/sv/ues/fia/graficadordatossensores/Graficador_Frame.java
+++ src/sv/ues/fia/graficadordatossensores/Graficador_Frame.java
@@ -13,17 +13,27 @@ public class Graficador_Frame extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jButton_graficar = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jButton_graficar.setText("Graficar");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(76, 76, 76)
+                .addComponent(jButton_graficar)
+                .addContainerGap(85, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(42, 42, 42)
+                .addComponent(jButton_graficar)
+                .addContainerGap(45, Short.MAX_VALUE))
         );
 
         pack();
@@ -31,5 +41,6 @@ public class Graficador_Frame extends javax.swing.JFrame {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton_graficar;
     // End of variables declaration//GEN-END:variables
 }
